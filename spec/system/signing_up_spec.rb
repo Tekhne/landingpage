aa28d9b43a226e2e6c8f91e301a879b0fbe7f9e2
Peter Havens <peter.havens@gmail.com>
2018-05-31 15:17:39 -0600

    Add test for successfully creating a new contact for user.

---
+++ spec/system/signing_up_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'signing up:' do
+  it 'successfully creates a new contact for user' do
+    visit root_path
+    expect(page).to have_current_path(root_path)
+
+    fill_in 'Email', with: 'jsmith@example.com'
+    click_button 'Subscribe now!'
+    expect(page).to have_current_path(contact_path)
+    expect(page).to have_css('[data-t-notice-success]')
+  end
+end
