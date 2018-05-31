f139c97102740283bdfdb71de54fab758a52834f
Peter Havens <peter.havens@gmail.com>
2018-05-31 14:08:53 -0600

    Implement Subscriptions#subscribe.

--- app/lib/subscriptions.rb
+++ app/lib/subscriptions.rb
@@ -1,9 +1,15 @@
 class Subscriptions
+  include LogsAndRaisesExceptions
+
   class Error < StandardError
     attr_accessor :original_exception
   end
 
   def subscribe(params)
-    # FIXME
+    Contact.create!(email: params.fetch(:email))
+  rescue ActiveRecord::RecordInvalid => exception
+    log_and_raise(exception, 'The information provided was invalid.')
+  rescue ActionController::ParameterMissing => exception
+    log_and_raise(exception, 'No email was provided.')
   end
 end
--- spec/lib/subscriptions_spec.rb
+++ spec/lib/subscriptions_spec.rb
@@ -2,6 +2,39 @@ require 'rails_helper'
 
 RSpec.describe Subscriptions do
   describe '#subscribe' do
-    # FIXME
+    let(:params) { ActionController::Parameters.new(email: 'user@example.com') }
+
+    subject(:subscriptions) { Subscriptions.new }
+
+    before do
+      allow(Contact).to receive(:create!)
+      allow(subscriptions).to receive(:log_and_raise)
+    end
+
+    it 'creates a new contact from given params' do
+      subscriptions.subscribe(params)
+      expect(Contact).to have_received(:create!).with(email: params[:email])
+    end
+
+    describe 'when new contact is invalid' do
+      it 'logs and raises exception' do
+        contact = build(:contact)
+        exception = ActiveRecord::RecordInvalid.new(contact)
+        allow(Contact).to receive(:create!) { raise exception }
+        subscriptions.subscribe(params)
+        expect(subscriptions).to \
+          have_received(:log_and_raise).with(exception, instance_of(String))
+      end
+    end
+
+    describe 'when given params is missing email' do
+      it 'logs and raises exception' do
+        exception = ActionController::ParameterMissing.new(:email)
+        allow(params).to receive(:fetch) { raise exception }
+        subscriptions.subscribe(params)
+        expect(subscriptions).to \
+          have_received(:log_and_raise).with(exception, instance_of(String))
+      end
+    end
   end
 end
